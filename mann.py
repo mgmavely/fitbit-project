894e77e451b65569c229c35d3a134b9529f7373c
mannp73 <59980884+mannp73@users.noreply.github.com>
2021-03-06 16:44:21 -0500

    alarm sound

--- mann.py
+++ mann.py
@@ -1,5 +1,15 @@
 # mann patel
+from playsound import playsound
+
+
 def time_and_level(data):
     date_time = data["sleep"][0]["levels"]["data"][-1]["dateTime"][11:19]
     level = data["sleep"][0]["levels"]["data"][-1]["level"]
     return [date_time, level]
+
+
+def play_alarm():
+    playsound('alarm.mp3')
+
+
+play_alarm()
