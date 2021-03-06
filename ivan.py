e8fb082a2ad505deb46c2da1e4c307c4cc069ece
ivantheratil <ivantheratil@gmail.com>
2021-03-06 02:02:11 -0500

    updated ivan.py

--- ivan.py
+++ ivan.py
@@ -1,3 +1,16 @@
-import requests
-import os
+
 from datetime import datetime as dt
+
+
+def date_and_time():
+    now = dt.now()
+
+    year = now.strftime("%Y")
+
+    month = now.strftime("%m")
+
+    day = now.strftime("%d")
+
+    date_time = now.strftime("%Y-%m-%d")
+    return date_time
+
