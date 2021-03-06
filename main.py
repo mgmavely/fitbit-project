2c5708d74bc2471dd9d298407b9770abc131103c
mgmavely <mgmavely@gmail.com>
2021-03-06 01:57:05 -0500

    fixed ivan's harness, added mann's

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from datetime import datetime as dt
 import ivan
+import mann
 
 date = "2021-02-14"
 access_token = os.environ.get("access_token")
@@ -10,4 +11,5 @@ headers = {"Authorization": "Bearer {}".format(access_token)}
 
 response = requests.get(url=endpoint, headers=headers).json()
 print(response)
-print(f"Ivan's function returned: {date_and_time()}")
+print(f"Ivan's function returned: {ivan.date_and_time()}")
+print(f"Mann's function returned: {mann.time_and_level(response)}")
