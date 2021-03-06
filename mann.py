8c802af9396a6f9f2b731496dc9953d5eee36f8f
mannp73 <59980884+mannp73@users.noreply.github.com>
2021-03-06 02:40:00 -0500

    time_and_level update mann.py

--- mann.py
+++ mann.py
@@ -1,1 +1,5 @@
-#mann patel
+# mann patel
+def time_and_level(data):
+    date_time = data["sleep"][0]["levels"]["data"][-1]["dateTime"][11:19]
+    level = data["sleep"][0]["levels"]["data"][-1]["level"]
+    return [date_time, level]
