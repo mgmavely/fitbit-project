c49153515ab23a43e7b844273e0129e376c0d1c8
ivantheratil <ivantheratil@gmail.com>
2021-03-06 02:17:24 -0500

    meow time is done

--- ivan.py
+++ ivan.py
@@ -5,12 +5,6 @@ from datetime import datetime as dt
 def date_and_time():
     now = dt.now()
 
-    year = now.strftime("%Y")
-
-    month = now.strftime("%m")
-
-    day = now.strftime("%d")
-
     date_time = now.strftime("%Y-%m-%d")
     return date_time
 
